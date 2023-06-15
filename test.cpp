17367476476735b287edbfbf5f15073d39e92883
Mohamed Makaty <81934834+makaty95@users.noreply.github.com>
2023-06-15 14:04:32 +0300

    UPDATE #1.2
    Modifying 'test.cpp' file

--- test.cpp
+++ test.cpp
@@ -1,19 +1,43 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+struct person{
+    int age;
+    string name;
+    string ID;
+    float weight;
+
+
+};
+
 int main()
 {
-
-    int a;
-    cin >> a;
-    while(a--)
+    int n;
+    cout << "Enter number of people:" << endl;
+    cin >> n;
+    person* People = new person[n];
+    int i = 0;
+    while(n--)
     {
-        int x,y;
-        cin >> x >> y;
-        if(x<y)
-        cout << "<" << endl;
-        else if(x == y)
-        cout << '=' << endl;
-        else cout << ">" << endl;
-        
+        cout << "PERSON #" << i+1 << endl;
+        cout << "Enter name: "; cin >> People[i].name;
+        cout << "Enter age: "; cin >> People[i].age;
+        cout << "Enter ID: "; cin >> People[i].ID;
+        cout << "Enter weight: "; cin >> People[i].weight;
+        cout << "-----------------------------------------" << endl;
+        i++;
+
+
     }
+    for(int j = 0; j<= i; j++)
+    {
+        cout << "PERSON #" << j+1 << endl;
+        cout << "NAME: " << People[j].name << endl;
+        cout << "AGE: " << People[j].age << endl;
+        cout << "ID: " << People[j].ID << endl;
+        cout << "WEIGHT: " << People[j].weight << endl;
+        cout << "========================================" << endl;
+
+    }
+
+    
 }
